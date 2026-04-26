e1a2016ed23cfd6e89140f5fc4689be42839534b
onixldlc <onxdl16427@outlook.com>
2026-04-27 03:43:39 +0700

    add: added my custom themes

---
+++ lua/custom/themes/catnord.lua
@@ -0,0 +1,78 @@
+-- catnord: nord bg/fg + catppuccin everything else
+-- visible nord bar (lightbg matches statusline_bg to prevent icon bg mismatch)
+local M = {}
+
+M.base_30 = {
+  -- NORD: bg + fg + bar
+  white = "#D8DEE9",
+  darker_black = "#242933",
+  black = "#2E3440",
+  black2 = "#343a46",
+  statusline_bg = "#333945",
+  lightbg = "#3F4551",
+
+  -- CATPPUCCIN: everything else
+  one_bg = "#2d2c3c",
+  one_bg2 = "#363545",
+  one_bg3 = "#3e3d4d",
+  
+  -- catpuccin grey:
+  -- grey = "#474656",
+  -- nord grey:
+  grey = "#4B515D",
+  
+  grey_fg = "#4e4d5d",
+  grey_fg2 = "#555464",
+  light_grey = "#605f6f",
+  red = "#F38BA8",
+  baby_pink = "#ffa5c3",
+  pink = "#F5C2E7",
+  line = "#383747",
+  green = "#ABE9B3",
+  vibrant_green = "#b6f4be",
+  nord_blue = "#8bc2f0",
+  blue = "#89B4FA",
+  yellow = "#FAE3B0",
+  sun = "#ffe9b6",
+  purple = "#d0a9e5",
+  dark_purple = "#c7a0dc",
+  teal = "#B5E8E0",
+  orange = "#F8BD96",
+  cyan = "#89DCEB",
+  pmenu_bg = "#ABE9B3",
+  folder_bg = "#89B4FA",
+  lavender = "#c7d1ff",
+}
+
+M.base_16 = {
+  -- NORD: bg + fg
+  base00 = "#2E3440",
+  base04 = "#D8DEE9",
+  base05 = "#E5E9F0",
+  base06 = "#ECEFF4",
+  base07 = "#8FBCBB",
+  -- CATPPUCCIN: rest
+  base01 = "#282737",
+  base02 = "#2f2e3e",
+  base03 = "#383747",
+  base08 = "#F38BA8",
+  base09 = "#F8BD96",
+  base0A = "#FAE3B0",
+  base0B = "#ABE9B3",
+  base0C = "#89DCEB",
+  base0D = "#89B4FA",
+  base0E = "#CBA6F7",
+  base0F = "#F38BA8",
+}
+
+M.polish_hl = {
+  ["@variable"] = { fg = M.base_30.lavender },
+  ["@property"] = { fg = M.base_30.teal },
+  ["@variable.builtin"] = { fg = M.base_30.red },
+}
+
+M.type = "dark"
+
+M = require("base46").override_theme(M, "catnord")
+
+return M
---
+++ lua/custom/themes/dracat.lua
@@ -0,0 +1,73 @@
+-- dracat: dracula bg/fg + catppuccin everything else
+-- visible dracula bar (lightbg matches statusline_bg)
+local M = {}
+
+M.base_30 = {
+  -- DRACULA: bg + fg + bar
+  white = "#F8F8F2",
+  darker_black = "#222430",
+  black = "#282A36",
+  black2 = "#2d303e",
+  statusline_bg = "#44475A",
+  lightbg = "#44475A",
+
+  -- CATPPUCCIN: everything else
+  one_bg = "#2d2c3c",
+  one_bg2 = "#363545",
+  one_bg3 = "#3e3d4d",
+  grey = "#474656",
+  grey_fg = "#4e4d5d",
+  grey_fg2 = "#555464",
+  light_grey = "#605f6f",
+  red = "#F38BA8",
+  baby_pink = "#ffa5c3",
+  pink = "#F5C2E7",
+  line = "#383747",
+  green = "#ABE9B3",
+  vibrant_green = "#b6f4be",
+  nord_blue = "#8bc2f0",
+  blue = "#89B4FA",
+  yellow = "#FAE3B0",
+  sun = "#ffe9b6",
+  purple = "#d0a9e5",
+  dark_purple = "#c7a0dc",
+  teal = "#B5E8E0",
+  orange = "#F8BD96",
+  cyan = "#89DCEB",
+  pmenu_bg = "#ABE9B3",
+  folder_bg = "#89B4FA",
+  lavender = "#c7d1ff",
+}
+
+M.base_16 = {
+  -- DRACULA: bg + fg
+  base00 = "#282A36",
+  base04 = "#F8F8F2",
+  base05 = "#f8f8f2",
+  base06 = "#f1f2f8",
+  base07 = "#f7f7fb",
+  -- CATPPUCCIN: rest
+  base01 = "#282737",
+  base02 = "#2f2e3e",
+  base03 = "#383747",
+  base08 = "#F38BA8",
+  base09 = "#F8BD96",
+  base0A = "#FAE3B0",
+  base0B = "#ABE9B3",
+  base0C = "#89DCEB",
+  base0D = "#89B4FA",
+  base0E = "#CBA6F7",
+  base0F = "#F38BA8",
+}
+
+M.polish_hl = {
+  ["@variable"] = { fg = M.base_30.lavender },
+  ["@property"] = { fg = M.base_30.teal },
+  ["@variable.builtin"] = { fg = M.base_30.red },
+}
+
+M.type = "dark"
+
+M = require("base46").override_theme(M, "dracat")
+
+return M
